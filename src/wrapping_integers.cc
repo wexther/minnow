ceade4e7b530bcb91455afa2f308342b33f836f4
wexther <923977588@qq.com>
2024-10-22 22:10:39 +0800

    更新报错信息

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -18,9 +18,11 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
   // (void)checkpoint;
   // return {};
   uint64_t ans = raw_value_ - zero_point.raw_value_ + ( checkpoint & 0xffffffff00000000 );
-  if ( ans < checkpoint && checkpoint - ans > 0x80000000 && ans < 0xffffffff00000000 )
+  if ( ans < checkpoint && checkpoint - ans > 0x80000000 ) {
+    if ( ans >= 0xffffffff00000000 )
+      throw( "wrap 2 uint64_t overflow" );
     ans += 0x100000000;
-  else if ( ans > checkpoint && ans - checkpoint > 0x80000000 && ans >= 0x100000000 )
+  } else if ( ans > checkpoint && ans - checkpoint > 0x80000000 && ans >= 0x100000000 )
     ans -= 0x100000000;
   return ans;
 }
